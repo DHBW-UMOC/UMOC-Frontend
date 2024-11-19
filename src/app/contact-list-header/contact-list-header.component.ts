9e4907d37763fa9313635469f585ddc68d09ab01
Adrian Schön <aschoen@united-internet.de>
2024-11-19 17:20:13 +0100

    Added contact-list-header component for 'Chats' text

---
+++ src/app/contact-list-header/contact-list-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-list-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-list-header.component.html',
+  styleUrl: './contact-list-header.component.scss'
+})
+export class ContactListHeaderComponent {
+
+}
--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ContactContainerComponent } from "../contact-container/contact-container.component";
 import { ContactListSearchBarComponent } from "../contact-list-search-bar/contact-list-search-bar.component";
+import { ContactListHeaderComponent } from "../contact-list-header/contact-list-header.component";
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [ContactContainerComponent, ContactListSearchBarComponent],
+  imports: [ContactContainerComponent, ContactListSearchBarComponent, ContactListHeaderComponent],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.scss'
 })
