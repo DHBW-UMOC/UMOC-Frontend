77be96c859a3d6f511ffee45963cdafcf5a844c2
Lucas Mohr <lucas.mohr@de.markant.com>
2024-11-20 14:54:26 +0100

    Dynamically load contacts and messages

--- src/app/chat-window/chat-window.component.ts
+++ src/app/chat-window/chat-window.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { MessageComponent } from "../message/message.component";
 import { ChatInputComponent } from "../chat-input/chat-input.component";
+import {CommonModule} from "@angular/common";
+import {Message} from "../model/message.model";
+import {ChatService} from "./chat.service";
 
 @Component({
   selector: 'app-chat-window',
   standalone: true,
-  imports: [MessageComponent, ChatInputComponent],
+  imports: [CommonModule, MessageComponent, ChatInputComponent],
   templateUrl: './chat-window.component.html',
   styleUrl: './chat-window.component.scss'
 })
-export class ChatWindowComponent {
+export class ChatWindowComponent implements OnInit {
+
+  protected messages: Array<Message> = [];
+
+  constructor(private chatService: ChatService) {}
+
+  ngOnInit(): void {
+    this.messages = this.chatService.fetchChatHistory("session")
+  }
 
 }
--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { ContactContainerComponent } from "../contact-container/contact-container.component";
 import { ContactListSearchBarComponent } from "../contact-list-search-bar/contact-list-search-bar.component";
 import { ContactListHeaderComponent } from "../contact-list-header/contact-list-header.component";
+import {ContactListService} from "./contact-list.service";
+import {Contact} from "../model/contact.model";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [ContactContainerComponent, ContactListSearchBarComponent, ContactListHeaderComponent],
+  imports: [ContactContainerComponent, ContactListSearchBarComponent, ContactListHeaderComponent, CommonModule],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.scss'
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit {
+
+  protected contacts: Array<Contact> = [];
+
+  constructor(private contactService: ContactListService) {}
+
+  ngOnInit(): void {
+    this.contacts = this.contactService.fetchContacts("session")
+  }
 
 }
