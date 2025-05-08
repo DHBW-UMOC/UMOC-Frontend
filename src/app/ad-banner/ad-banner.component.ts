dff02ae0a3a61f124b46e65acd83073a3a075ba2
Adrian Schön <aschoen@united-internet.de>
2025-05-08 14:01:29 +0200

    Added Developer Focus

--- src/app/ad-banner/ad-banner.component.ts
+++ src/app/ad-banner/ad-banner.component.ts
@@ -5,8 +5,11 @@ import { NgIf } from '@angular/common';
   selector: 'app-ad-banner',
   imports: [NgIf],
   templateUrl: './ad-banner.component.html',
-  styleUrl: './ad-banner.component.scss'
+  styleUrls: ['./ad-banner.component.scss']
 })
 export class AdBannerComponent {
   @Input() hidden: boolean = false;
+  @Input() imageSrc?: string;
+  @Input() videoSrc?: string;
+  @Input() altText: string = 'Ad Banner';
 }
