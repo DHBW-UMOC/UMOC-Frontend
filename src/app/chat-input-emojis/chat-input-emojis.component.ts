d421a473e7798449b7e11048c5f33f88123397ac
Adrian Schön <aschoen@united-internet.de>
2025-05-14 13:52:52 +0200

    Mat Icons for extra / emojis

--- src/app/chat-input-emojis/chat-input-emojis.component.ts
+++ src/app/chat-input-emojis/chat-input-emojis.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-chat-input-emojis',
-  imports: [],
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './chat-input-emojis.component.html',
   styleUrl: './chat-input-emojis.component.scss'
 })
 export class ChatInputEmojisComponent {
-
 }
