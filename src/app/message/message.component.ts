2dae8515e2d3c5231f240b2d03e6d24fbe434eaa
Adrian Schön <aschoen@united-internet.de>
2025-05-14 13:29:03 +0200

    Username color based on hash from username

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -12,4 +12,17 @@ export class MessageComponent {
   @Input() message: Message | undefined;
   @Input() currentUser: string | undefined = '';
   @Input() isGroupChat: boolean = false;
+
+  stringToColour(str: string): string {
+    let hash = 0;
+    str.split('').forEach(char => {
+      hash = char.charCodeAt(0) + ((hash << 5) - hash)
+    })
+    let colour = '#'
+    for (let i = 0; i < 3; i++) {
+      const value = (hash >> (i * 8)) & 0xff
+      colour += value.toString(16).padStart(2, '0')
+    }
+    return colour
+  }
 }
