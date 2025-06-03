25889245131e757bc8fed1d5a86b03157b88f45e
Adrian Schön <aschoen@united-internet.de>
2025-06-03 09:27:27 +0200

    EarlyMessage now appears as chat bubble

--- src/app/model/early-message.model.ts
+++ src/app/model/early-message.model.ts
@@ -1,3 +1,5 @@
+import { Message } from './message.model';
+
 export class EarlyMessage {
   content: string;
   sender_id: string;
@@ -7,5 +9,17 @@ export class EarlyMessage {
     this.content = content;
     this.sender_id = sender_id;
     this.sender_username = sender_username;
+  }
+
+  toMessage(): Message {
+    return {
+      message_id: 'temp_' + Date.now(),
+      content: this.content,
+      sender_user_id: this.sender_id,
+      sender_username: this.sender_username,
+      timestamp: new Date(),
+      recipient_id: '',
+      type: 'text'
+    };
   }
 }
